e133c96e486e0fe3b9368e669c6443c64d560036
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-07-05 11:33:14 -0400

    Add warning for number of CPUs used by parallel

--- 01_scripts/01_blast_against_swissprot.sh
+++ 01_scripts/01_blast_against_swissprot.sh
@@ -7,6 +7,7 @@ SWISSPROT_HITS=04_blast_results/analyzed_genes.hits
 SWISSPROT_DB=~/Software/blastplus_databases/swissprot
 
 # Blast all sequences against swissprot (must be installed locally)
+# WARNING use `-j N` if you need to limit the number of CPUs used to N <integer>
 cat $SEQUENCE_FILE | parallel -k --block 1k --recstart '>' --pipe 'blastx -db '$SWISSPROT_DB' -query - -evalue 1e-3 -outfmt 6 -max_target_seqs 1' > $SWISSPROT_RESULT
 
 # TODO filter blasts on similarity, evalue, length...
